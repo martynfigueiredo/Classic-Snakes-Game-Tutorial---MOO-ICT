13c602ba7966dc79866b11ef959dfea0bfbfe88b
mmartyndev <hello@mmartyn.dev>
2022-10-13 18:11:15 -0300

    Add project files.

---
+++ Classic Snakes Game Tutorial - MOO ICT/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classic_Snakes_Game_Tutorial___MOO_ICT
+{
+    internal class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static string directions;
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            directions = "left";
+        }
+
+
+    }
+}
